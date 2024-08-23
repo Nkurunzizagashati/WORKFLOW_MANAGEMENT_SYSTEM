88051468050b9db7e650b3f880392700c6502d49
Fabrice <f.nkurunziz@alustudent.com>
2024-08-23 10:43:33 +0200

    define login and register pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:workflow_management_system/pages/auth/login_page.dart';
+import 'package:workflow_management_system/pages/auth/register_page.dart';
+import 'package:workflow_management_system/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "WORKFLOW MANAGEMENT SYSTEM",
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const HomePage(),
+        '/register': (context) => const RegisterPage(),
+        '/login': (context) => const LoginPage(),
+      },
+    );
+  }
 }
